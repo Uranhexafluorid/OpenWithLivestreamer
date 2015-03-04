0cef17b965e1bb6b0972746332cef62f23112e8b
Niklas Bammann <niklas.mac@googlemail.com>
2015-03-04 18:30:18 +0100

    Initial commit

---
+++ OpenWithLivestreamer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWithLivestreamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                string strCmdText;
+                strCmdText = args[0] + " source";
+                System.Diagnostics.Process.Start("livestreamer", strCmdText);
+            }
+            else
+            {
+                Console.WriteLine("No URL given");
+                Console.ReadLine();
+            }
+        }
+    }
+}
